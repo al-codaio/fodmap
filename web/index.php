50cbf60d4b3ee3683ebc8a41a6e37c4e0724e8e3
Al Chen <atc239@gmail.com>
2019-03-13 15:25:24 -0400

    sorted rows to last 20 created

--- web/index.php
+++ web/index.php
@@ -1,21 +1,6 @@
 <?php
 
 require('../vendor/autoload.php');
-$coda = new CodaPHP\CodaPHP('75764dec-d6fc-4a72-9e56-8efe8be59301');
-$docID = 'BvPGe-8_sj';
-$tableID = 'grid-r5hUgXQ8WA';
-
-$table = $coda->getTable($docID, $tableID);
-// $rows = $coda->listRows($docID, $tableID, ['query' => array('Food Group' => 'Grains/Starches')]);
-$rows = $coda->listRows($docID, $tableID, ['limit' => 25]);
-
-// echo "<pre>";
-// echo json_encode($rows, JSON_PRETTY_PRINT);
-// echo "</pre>";
-
-// var_dump(json_encode($result, JSON_PRETTY_PRINT));
-
-
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -30,11 +15,31 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 // Our web handlers
-
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig' array(
-  	'rows' => $rows;
+  
+  $coda = new CodaPHP\CodaPHP('75764dec-d6fc-4a72-9e56-8efe8be59301');
+  $docID = 'BvPGe-8_sj';
+  $tableID = 'grid-r5hUgXQ8WA';
+  $table = $coda->getTable($docID, $tableID);
+  
+  //list rows with query not working below 
+  // $rows = $coda->listRows($docID, $tableID, ['query' => array('Food Group' => 'Grains/Starches')]);
+  $rows = $coda->listRows($docID, $tableID)['items'];
+  $sorted_rows = [];
+  foreach ($rows as $row) {
+  	if ($row['index'] > count($rows) - 20) {
+  	  array_push($sorted_rows, $row);
+  	}
+  }
+
+  // echo "<pre>";
+  // echo json_encode($sorted_rows, JSON_PRETTY_PRINT);
+  // echo "</pre>";
+
+
+  return $app['twig']->render('index.twig', array(
+  	'rows' => $sorted_rows
   ));
 });
 
