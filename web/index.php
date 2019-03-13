cfd095012cb1d3b67f4ea90f2074d0d302a3afb6
Al Chen <atc239@gmail.com>
2019-03-13 12:30:40 -0400

    added CodaPHP library

--- web/index.php
+++ web/index.php
@@ -1,6 +1,20 @@
 <?php
 
 require('../vendor/autoload.php');
+$coda = new CodaPHP\CodaPHP('75764dec-d6fc-4a72-9e56-8efe8be59301');
+$docID = 'BvPGe-8_sj';
+$tableID = 'grid-r5hUgXQ8WA';
+
+$table = $coda->getTable($docID, $tableID);
+// $rows = $coda->listRows($docID, $tableID, ['query' => array('Food Group' => 'Grains/Starches')]);
+$rows = $coda->listRows($docID, $tableID, ['limit' => 25]);
+
+// echo "<pre>";
+// echo json_encode($rows, JSON_PRETTY_PRINT);
+// echo "</pre>";
+
+// var_dump(json_encode($result, JSON_PRETTY_PRINT));
+
 
 $app = new Silex\Application();
 $app['debug'] = true;
